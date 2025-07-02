1574029fdee8d5baebe7ba692c1cad04e15e9f07
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-07-02 12:59:22 +0200

    initial commit

---
+++ src/main/java/io/erisdev/secretlink/SecretLinkApplication.java
@@ -0,0 +1,13 @@
+package io.erisdev.secretlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/erisdev/secretlink/SecretLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erisdev.secretlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
